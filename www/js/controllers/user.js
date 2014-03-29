cd6bfff01638c22632daa0a038bad29a1a6aff45
Mark Peace <mark@learnsomestuff.com>
2014-03-29 14:57:09 +0000

    basic drifttext

--- www/js/controllers/user.js
+++ www/js/controllers/user.js
@@ -1,3 +1,21 @@
 xyxxy.controller('UserSplash', function($scope, ParseService) { 
   
+  var kineticHolder = document.getElementById("kineticHolder");
+  
+  function driftyText() {  
+    var n = document.createElement("span");
+    n.className="textCloud"
+    n.style.top= (Math.random()*100) + "%"
+    n.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus nisi metus, sit amet congue tortor fringilla quis"
+    kineticHolder.appendChild(n);
+   
+    setTimeout(function() {
+      driftyText();
+    },2000);
+    
+  }
+  
+  
+  driftyText()
+  
 });
