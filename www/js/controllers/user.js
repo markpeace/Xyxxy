22dd5083809652152425b8c32836a36b4df59eee
Mark Peace <mark@learnsomestuff.com>
2014-03-30 15:32:23 +0000

    driftytext

--- www/js/controllers/user.js
+++ www/js/controllers/user.js
@@ -1,21 +1,47 @@
 xyxxy.controller('UserSplash', function($scope, ParseService) { 
   
-  var kineticHolder = document.getElementById("kineticHolder");
   
-  function driftyText() {  
-    var n = document.createElement("span");
-    n.className="textCloud"
-    n.style.top= (Math.random()*100) + "%"
-    n.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus nisi metus, sit amet congue tortor fringilla quis"
-    kineticHolder.appendChild(n);
-   
-    setTimeout(function() {
-      driftyText();
-    },2000);
+  driftyText = function () {
     
-  }
+    totalClouds=0
+    
+    layers = [
+      {label:'top', speed:10},
+      {label:'middle', speed:20},
+      {label:'bottom', speed:30}
+    ];  
+    
+    textCloud = function () {
+      
+      var layer=Math.floor(Math.random()*3)
+      c = document.createElement("span");
+      c.className="textCloud " + layers[layer].label
+      c.speed=layers[layer].speed
+      c.id="m"+Math.round((Math.random()*1000))
+      c.style.top= (Math.random()*100) + "%"
+      c.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus nisi metus, sit amet congue tortor fringilla quis"
+      return c
+    }
+    
+    
+    
+    var createTimer = setInterval(function() { 
+      
+      if (totalClouds<30) { 
+        
+        totalClouds++;
+        tc=new textCloud()
+        kineticHolder.appendChild(tc); 
+        var destroyTimer = setTimeout(function() {  
+          if (document.getElementById(tc.id)){ tc.parentNode.removeChild(tc)} 
+          totalClouds--;
+        },tc.speed*1000)  
+        }
+    },2000)
+    
+    
+    
+    }();
   
-  
-  driftyText()
   
 });
